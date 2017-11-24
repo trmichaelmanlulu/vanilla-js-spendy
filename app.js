d78583cf6452a84a0faa5dc7bbeb7295efcf2dd6
trmichaelmanlulu <michael.manlulu@tooltwist.com>
2017-11-24 09:54:11 +0800

    added html, css and js file

---
+++ app.js
@@ -0,0 +1,63 @@
+
+// BUDGET CONTROLLER
+var budgetController = (function() {
+    
+        // something cool
+    
+    })();
+    
+    
+    // UI CONTROLLER
+    var UIController = (function() {
+    
+        var DOMstrings = {
+            inputType: '.add__type',
+            inputDescription: '.add__description',
+            inputValue: '.add__value',
+            inputBtn: '.add__btn'
+        }
+    
+        return {
+            getInput: function() {
+                return {
+                    type: document.querySelector(DOMstrings.inputType).value, // + or -
+                    description: document.querySelector(DOMstrings.inputDescription).value,
+                    value: document.querySelector(DOMstrings.inputValue).value
+                }
+            }, 
+            getDOMstrings: function() {
+                return DOMstrings;
+            }
+        }
+    
+    })();
+    
+    
+    // GLOBAL APP CONTROLLER
+    var controller = (function(budgetCtrl, UICtrl) {
+    
+        var DOM = UICtrl.getDOMstrings();
+    
+        var ctrlAddItem = function() {
+            // 1. Get the field input data
+            var input = UICtrl.getInput();
+            console.log(input);
+            // 2. Add the item to the budget controller
+    
+            // 3. Add the item to the UI
+    
+            // 4. Calculate the budget
+    
+            // 5. Display the budget on the UI
+        }
+    
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    
+        document.addEventListener('keypress', function(event) {
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    
+    })(budgetController, UIController);
+    
